32d5e1d2f3cd1e4ac9fd655a8b3598a8681226fd
Alex <zemzikova.oleksandra@gmail.com>
2025-01-07 20:42:15 +0200

    start

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { readFile } from 'fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  const fileData = readFile(PATH_DB);
+  return JSON.parse(fileData);
+};
