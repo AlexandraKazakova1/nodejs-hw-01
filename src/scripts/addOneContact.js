7965580e840208c9157e79a8b50d5676b2068c68
Alex <zemzikova.oleksandra@gmail.com>
2025-01-10 20:25:00 +0200

    4-8

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,14 +1,18 @@
 import { PATH_DB } from '../constants/contacts.js';
 import { createFakeContact } from '../utils/createFakeContact.js';
 import fs from 'node:fs/promises';
-import { readContacts } from '../utils/readContacts.js';
 
 export const addOneContact = async () => {
-  const dataOneNewContact = JSON.createFakeContact();
+  const dataOneNewContact = createFakeContact();
 
   try {
-    await fs.appendFile(PATH_DB, dataOneNewContact, 'utf-8');
-    console.log('Дані успішно додані до файлу.');
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data || '[]');
+
+    contacts.push(dataOneNewContact);
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2), 'utf-8');
+    console.log('Новий контакт успішно додано до файлу.');
   } catch (error) {
     console.error('Помилка додавання даних до файлу:', error.message);
   }
